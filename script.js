be17ab8edde316e5abcc488b9e8f1ac9185b41f4
Frank Buono <fbuonojr@fordham.edu>
2020-07-29 16:05:53 -0400

    start button

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var questionArr = [{Q: "", A: ["", "", "", ""]}];
+var questionArr = [{Q: "hi", A: ["", "", "", ""]}];
 
 $("quiz-screen").hide();
 
 $("#start-button").on("click", function(){
     $("#start-screen").hide();
+
+    $("#question").text(questionArr[0].Q);
     $("#quiz-screen").show();
 });
